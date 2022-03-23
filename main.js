ead000d56d3a97609586d287c37764ab982cd3f4
loic <l.dauchy@codeur.online>
2022-03-23 10:52:14 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+window.onload = () => {
+    setTimeout(() => {
+        document.getElementById('loadingAnimation').style.display = "none";
+        document.getElementById('displayContent').style.display = "block";
+    }, 3000);
+}
